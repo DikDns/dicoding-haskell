07e9c3eea4cc202c9aef7a66d569145e9b65c307
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2023-10-19 08:05:46 +0700

    add: kuis-1

---
+++ kuis-1.hs
@@ -0,0 +1,7 @@
+main = do
+    let nilaiSiswa = 100
+    if nilaiSiswa >= 80 then print("A")
+    else if nilaiSiswa >= 60 then print("B")
+    else if nilaiSiswa >= 40 then print("C")
+    else if nilaiSiswa >= 20 then print("D")
+    else print("E")
